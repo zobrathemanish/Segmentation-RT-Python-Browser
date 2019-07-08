8dabe55f767ef0f591bef4d104e4a3ac27bd0b29
zobrathemanish <manish@bottle.com.np>
2019-07-08 15:24:11 +0545

    first commit

--- camera.py
+++ camera.py
@@ -1,14 +1,62 @@
+import cv2
+import imutils
 import time
-from base_camera import BaseCamera
+import numpy as np
+
+class VideoCamera(object):
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        # self.video = cv2.VideoCapture(0)
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        self.video = cv2.VideoCapture('cropvideo.mp4')
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+
+        CLASSES = open('enet-cityscapes/enet-classes.txt').read().strip().split("\n")
+
+        # if a colors file was supplied, load it from disk
+
+        COLORS = open('enet-cityscapes/enet-colors.txt').read().strip().split("\n")
+        COLORS = [np.array(c.split(",")).astype("int") for c in COLORS]
+        COLORS = np.array(COLORS, dtype="uint8")
+        
+        net = cv2.dnn.readNet('enet-cityscapes/enet-model.net')
+
+        (grabbed, frame) = self.video.read()
+
+        frame = imutils.resize(frame, width=1000)
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (1024, 512), 0,
+            swapRB=True, crop=False)
+        net.setInput(blob)
+        start = time.time()
+        output = net.forward()
+        end = time.time()
+
+        (numClasses, height, width) = output.shape[1:4]
+
+        # our output class ID map will be num_classes x height x width in
+        # size, so we take the argmax to find the class label with the
+        # largest probability for each and every (x, y)-coordinate in the
+        # image
+        classMap = np.argmax(output[0], axis=0)
+
+        mask = COLORS[classMap]
+
+        # resize the mask such that its dimensions match the original size
+        # of the input frame
+        mask = cv2.resize(mask, (frame.shape[1], frame.shape[0]),
+            interpolation=cv2.INTER_NEAREST)
+
+        # perform a weighted combination of the input frame with the mask
+        # to form an output visualization
+        output = ((0.15 * frame) + (0.7 * mask)).astype("uint8")
 
 
-class Camera(BaseCamera):
-    """An emulated camera implementation that streams a repeated sequence of
-    files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
-    imgs = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
-
-    @staticmethod
-    def frames():
-        while True:
-            time.sleep(1)
-            yield Camera.imgs[int(time.time()) % 3]
+        ret, jpeg = cv2.imencode('.jpg', output)
+        return jpeg.tobytes()
